7307a425032612ddbd811aea58c2c16a613edf91
Lenchis <naa2015st@mail.ru>
2025-02-07 10:01:44 +0300

    commit 2

--- app/src/main/java/com/example/lab6/MainActivity.kt
+++ app/src/main/java/com/example/lab6/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.lab6
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.lab6.databinding.FragmentMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
+
 //Отображаем созданный Fragment
             supportFragmentManager.beginTransaction()
                 .add(R.id.fragment_container_view, MainFragment())
@@ -18,6 +24,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    class MainFragment : Fragment() {
+        private var _binding: FragmentMainBinding? = null
+        private var binding get() = _binding!!
 
+        override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+        ): View? {
+            _binding = FragmentMainBinding.inflate(inflater, container, false)
+            binding.textView.text = "Hello World!"
+            return binding.root
+        }
+
+        override fun onDestroyView() {
+            super.onDestroyView()
+            _binding = null
+        }
+
+    }
 
 }
